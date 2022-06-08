b70719bc1cd892941fcaf84fe9f100f07c6d627e
valmiraguiar <valmiir.aguiar7@gmail.com>
2022-06-08 13:01:47 -0300

    views added

--- src/main/java/com/ufes/pss/gestaofuncionarios/Principal.java
+++ src/main/java/com/ufes/pss/gestaofuncionarios/Principal.java
@@ -3,13 +3,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package com.ufes.pss.gestaofuncionarios.gestaodefuncionarios;
+package com.ufes.pss.gestaofuncionarios;
 
 /**
  *
  * @author Valmir Aguiar
  */
-public class GestaoDeFuncionarios {
+public class Principal {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
