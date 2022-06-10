f5a8a8cccaec4b363fefb6d9fffb899203f490dc
Andrico <andricominarrini@gmail.com>
2022-06-10 15:48:56 -0300

    Presenter Added

--- src/main/java/com/ufes/pss/gestaofuncionarios/presenter/BuscarFuncionarioPresenter.java
+++ src/main/java/com/ufes/pss/gestaofuncionarios/presenter/BuscarFuncionarioPresenter.java
@@ -2,7 +2,11 @@ package com.ufes.pss.gestaofuncionarios.presenter;
 
 import com.ufes.pss.gestaofuncionarios.collection.FuncionarioCollection;
 import com.ufes.pss.gestaofuncionarios.factory.SistemaDeLogs;
+import com.ufes.pss.gestaofuncionarios.model.Funcionario;
 import com.ufes.pss.gestaofuncionarios.view.BuscarFuncionarioView;
+import java.awt.event.ActionEvent;
+import javax.swing.JOptionPane;
+import javax.swing.ListSelectionModel;
 import javax.swing.table.DefaultTableModel;
 
 public class BuscarFuncionarioPresenter {
@@ -19,7 +23,110 @@ public class BuscarFuncionarioPresenter {
         view = new BuscarFuncionarioView();
         principal.getView().getDesktop().add(view);
         
-        tmFuncionarios = new DefaultTableModel(new Object[][]{}, new String[]{"ID", "Nome", "Idade", "Função", "Salário base (R$)"});
+        tmFuncionarios = new DefaultTableModel(
+                new Object[][]{},
+                new String[]{"ID", "Nome", "Idade", "Função", "Salário base (R$)"}
+        ) {
+            @Override
+            public boolean isCellEditable(final int row, final int column) {
+                return false;
+            }
+        };
+
+        view.getTblFuncionarios().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        view.getTblFuncionarios().setModel(tmFuncionarios);
+
+        view.getBtnFechar().addActionListener((ActionEvent ae) -> {
+            fechar(principal);
+        });
+
+        view.getBtnBusca().addActionListener((ActionEvent ae) -> {
+            buscar(funcionarios);
+        });
+
+        view.getBtnNovo().addActionListener((ActionEvent ae) -> {
+            novo(principal, funcionarios);
+        });
+
+        view.getBtnBonus().addActionListener((ActionEvent ae) -> {
+            if (view.getTblFuncionarios().getSelectedRow() < 0) {
+                JOptionPane.showMessageDialog(view, "Selecione uma linha");
+            } else {
+                bonus(principal, funcionarios);
+            }
+        });
+
+        view.getBtnVisualizar().addActionListener((ActionEvent ae) -> {
+            if (view.getTblFuncionarios().getSelectedRow() < 0) {
+                JOptionPane.showMessageDialog(view, "Selecione uma linha");
+            } else {
+                visualizar(principal, funcionarios);
+            }
+        });
+
+        view.setVisible(true);
+    }
+
+    private void fechar(PrincipalPresenter pp) {
+        view.dispose();
+        pp.updateNumFuncionarios();
+    }
+
+    private void buscar(FuncionarioCollection funcionarios) {
+
+        eraseTable();
+
+        String busca = view.getTxtBusca().getText().toLowerCase();
+
+        if (busca != null && !"".equals(busca)) {
+
+            for (Funcionario f : funcionarios.getFuncionarios()) {
+                if (f.getNome().toLowerCase().contains(busca)) {
+                    tmFuncionarios.addRow(new Object[]{
+                        f.getId(),
+                        f.getNome(),
+                        f.getIdade(),
+                        f.getCargo(),
+                        String.format("%.2f", f.getSalario()).replace(".", ",")
+                    });
+                }
+            }
+        } else {
+            String msg = "Forneça um nome para busca!";
+            logs.getLogger().logFalha("Busca", msg);
+            throw new RuntimeException(msg);
+        }
+
+        view.getTblFuncionarios().setModel(tmFuncionarios);
+    }
+
+    private void novo(PrincipalPresenter principal, FuncionarioCollection funcionarios) {
+        ManterFuncionarioPresenter manterFuncionarioPresenter = new ManterFuncionarioPresenter(principal, funcionarios, logs);
+        eraseTable();
+    }
+
+    private void bonus(PrincipalPresenter principal, FuncionarioCollection funcionarios) {
+        int id = Integer.parseInt(view.getTblFuncionarios().getValueAt(view.getTblFuncionarios().getSelectedRow(), 0).toString());
+        Funcionario f = funcionarios.findById(id);
+
+        new VerBonusPresenter(principal, f, logs);
+        eraseTable();
+        view.getTxtBusca().setText("");
+    }
+
+    private void visualizar(PrincipalPresenter principal, FuncionarioCollection funcionarios) {
+        int id = Integer.parseInt(view.getTblFuncionarios().getValueAt(view.getTblFuncionarios().getSelectedRow(), 0).toString());
+        Funcionario f = funcionarios.findById(id);
+        new ManterFuncionarioPresenter(principal, funcionarios, f, logs);
+        eraseTable();
+        view.getTxtBusca().setText("");
+    }
+
+    private void eraseTable() {
+        tmFuncionarios.setNumRows(0);
+        view.getTblFuncionarios().setModel(tmFuncionarios);
+
     }
     
 }
