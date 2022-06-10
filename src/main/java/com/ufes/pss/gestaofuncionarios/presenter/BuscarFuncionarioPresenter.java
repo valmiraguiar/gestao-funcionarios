f35b26b1043d601c0b75492a040980a28b70ef10
Andrico <andricominarrini@gmail.com>
2022-06-10 15:39:08 -0300

    Update BuscarFuncionarioPresenter.java

--- src/main/java/com/ufes/pss/gestaofuncionarios/presenter/BuscarFuncionarioPresenter.java
+++ src/main/java/com/ufes/pss/gestaofuncionarios/presenter/BuscarFuncionarioPresenter.java
@@ -1,13 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.ufes.pss.gestaofuncionarios.presenter;
 
-/**
- *
- * @author Andrico
- */
+import com.ufes.pss.gestaofuncionarios.collection.FuncionarioCollection;
+import com.ufes.pss.gestaofuncionarios.factory.SistemaDeLogs;
+import com.ufes.pss.gestaofuncionarios.view.BuscarFuncionarioView;
+import javax.swing.table.DefaultTableModel;
+
 public class BuscarFuncionarioPresenter {
+    
+    private BuscarFuncionarioView view;
+    private final DefaultTableModel tmFuncionarios;
+    private final FuncionarioCollection funcionarios;
+    private SistemaDeLogs logs;
+    
+    public BuscarFuncionarioPresenter(PrincipalPresenter principal, FuncionarioCollection funcionarios, SistemaDeLogs logs){
+        this.funcionarios = funcionarios;
+        this.logs = logs;
+        funcionarios.ordenar();
+        view = new BuscarFuncionarioView();
+        principal.getView().getDesktop().add(view);
+        
+        tmFuncionarios = new DefaultTableModel(new Object[][]{}, new String[]{"ID", "Nome", "Idade", "Função", "Salário base (R$)"});
+    }
     
 }
