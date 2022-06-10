b9c68a3981265d103b34a107f769cb69f098a47d
Andrico <andricominarrini@gmail.com>
2022-06-10 15:09:09 -0300

    Update PrincipalPresenter.java

--- src/main/java/com/ufes/pss/gestaofuncionarios/presenter/PrincipalPresenter.java
+++ src/main/java/com/ufes/pss/gestaofuncionarios/presenter/PrincipalPresenter.java
@@ -3,6 +3,7 @@ package com.ufes.pss.gestaofuncionarios.presenter;
 import com.ufes.pss.gestaofuncionarios.view.PrincipalView;
 import com.ufes.pss.gestaofuncionarios.collection.FuncionarioCollection;
 import com.ufes.pss.gestaofuncionarios.factory.SistemaDeLogs;
+import java.awt.event.ActionEvent;
 
 public class PrincipalPresenter {
     
@@ -12,6 +13,45 @@ public class PrincipalPresenter {
     
     public void inicializa(){
         
+        view = new PrincipalView();
+        informacoesDoSistema();
+        view.setVisible(true);
+        
+        view.getBtnAddFuncionario().addActionListener(((ActionEvent ae) -> {
+            addFuncionario();
+        }));
+        
+        view.getBtnBuscarFuncionario().addActionListener((ActionEvent ae) -> {
+            buscarFuncionario();
+        });
+        
+        view.getBtnCalcularSalario().addActionListener((ActionEvent ae) -> {
+            calcularSalario();
+        });
+        
+        view.getBtnAlterarLogs().addActionListener((ActionEvent ae) -> {
+            alterarLogs();
+        });
+    }
+
+    private void informacoesDoSistema() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private void addFuncionario() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private void buscarFuncionario() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private void calcularSalario() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private void alterarLogs() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
